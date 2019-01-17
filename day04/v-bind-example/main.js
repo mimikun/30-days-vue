be1d6040645311f8609196baf11179844b6619c9
mimikun <karaage_nennen@icloud.com>
2019-01-17 21:53:59 +0900

    v-bind example done

---
+++ day04/v-bind-example/main.js
@@ -0,0 +1,11 @@
+new Vue({
+  el: '#app',
+  data () {
+    return {
+      greeting: 'Hello World!',
+      user: 'mimikun',
+      city: 'Fukui',
+      cityImage: 'https://bit.ly/2SKJPqJ'
+    }
+  }
+});
