857491901bd3cbc6f1e989b8d029c4aaf0946ac0
mimikun <karaage_nennen@icloud.com>
2019-02-04 21:29:25 +0900

    day11 done

--- day11/main.js
+++ day11/main.js
@@ -25,6 +25,49 @@ const tweets = [
   }
 ];
 
+Vue.component("tweet-component", {
+  template: `
+    <div class="tweet">
+      <div class="box">
+        <article class="media">
+          <div class="media-left">
+            <figure class="image is-64x64">
+              <img src="./images/james.png">
+            </figure>
+          </div>
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong>James</strong>
+                <small>@jokerjames</small>
+                <br>
+                If you don't succeed, dust yourself off...
+              </p>
+            </div>
+            <div class="level-left">
+              <a class="level-item">
+                <span class="icon is-small">
+                  <i class="fas fa-heart"></i>
+                </span>
+                <span class="likes">
+                  10
+                </span>
+              </a>
+            </div>
+          </div>
+        </article>
+      </div>
+      <div class="control has-icons-left">
+        <input class="input is-small"
+          placeholder="Tweet your reply..." />
+        <span class="icon is-small is-left">
+          <i class="fas fa-envelope"></i>
+        </span>
+      </div>
+    </div>
+  `
+});
+
 new Vue({
   el: '#app',
   data: {
