c0464baec1c6d4ee466424e2fe4b9b7be3d5910b
mimikun <karaage_nennen@icloud.com>
2019-02-15 23:19:53 +0900

    vuex-store-example done

---
+++ day22/vuex-store-example/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <NumberDisplay></NumberDisplay>
+    <NumberSubmit></NumberSubmit>
+  </div>
+</template>
+
+<script>
+import NumberDisplay from './components/NumberDisplay.vue'
+import NumberSubmit from './components/NumberSubmit.vue'
+
+export default {
+  name: 'app',
+  components: {
+    NumberDisplay,
+    NumberSubmit
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ day22/vuex-store-example/src/components/NumberSubmit.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input v-model="numberInput" type="number" />
+    <button @click="addNumber(numberInput)">
+      Add new Number
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NumberSubmit",
+  data () {
+    return {
+      numberInput: 0
+    };
+  },
+  methods: {
+    addNumber(numberInput) {
+      this.$store.dispatch(
+        "addNumber",
+        Number(numberInput)
+      );
+    }
+  }
+};
+</script>
