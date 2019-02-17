29aaa5b4a51a9cc80347d1e79f4fab3406c10ca8
mimikun <karaage_nennen@icloud.com>
2019-02-17 19:38:09 +0900

    day24 done

--- day24/pokemon-routing/src/router.js
+++ day24/pokemon-routing/src/router.js
@@ -1,21 +1,21 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import BlastoiseCard from './components/BlastoiseCard.vue'
-import CharizardCard from './components/CharizardCard.vue'
-import VenusaurCard from './components/VenusaurCard.vue'
-import NotFound from './components/NotFound'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import BlastoiseCard from "./components/BlastoiseCard";
+import CharizardCard from "./components/CharizardCard";
+import VenusaurCard from "./components/VenusaurCard";
+import NotFound from "./components/NotFound";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-  { path: '/', component: CharizardCard },
-  { path: '/charizard', component: CharizardCard },
-  { path: '/blastoise', component: BlastoiseCard },
-  { path: '/venusaur', component: VenusaurCard },
-  { path: '*', component: NotFound }
+  { path: "/", component: CharizardCard },
+  { path: "/charizard", component: CharizardCard },
+  { path: "/blastoise", component: BlastoiseCard },
+  { path: "/venusaur", component: VenusaurCard },
+  { path: "*", component: NotFound }
 ];
 
-export const router = new VueRouter({
-  mode: 'history',
+export default new VueRouter({
+  mode: "history",
   routes
 });
