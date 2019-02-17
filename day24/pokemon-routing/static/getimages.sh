f4d8f8168ea24720d1424bf326037b7bad602b6d
mimikun <karaage_nennen@icloud.com>
2019-02-17 19:46:31 +0900

    Add getimage.sh

---
+++ day24/pokemon-routing/static/getimages.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "wget blastoise images"
+wget https://raw.githubusercontent.com/fullstackio/30-days-of-vue/master/day-24/src/pokemon-routing/static/blastoise.png
+
+echo "wget charizard images"
+wget https://raw.githubusercontent.com/fullstackio/30-days-of-vue/master/day-24/src/pokemon-routing/static/charizard.png
+
+echo "wget venusaur images"
+wget https://raw.githubusercontent.com/fullstackio/30-days-of-vue/master/day-24/src/pokemon-routing/static/venusaur.png
